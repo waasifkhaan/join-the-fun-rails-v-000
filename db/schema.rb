7269ba8716665b179e0edba731683029fcec51c6
Wasif Khan <waasif.khaan@gmail.com>
2018-10-04 16:35:11 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181004163158) do
+ActiveRecord::Schema.define(version: 20181004162923) do
 
   create_table "passengers", force: :cascade do |t|
     t.datetime "created_at", null: false
